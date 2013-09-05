5b20f0b1daca95c58be60c041931b01b8e1f93b4
Paul Griffiths <mail@paulgriffiths.net>
2013-09-05 10:33:01 -0400

    Add auto-generated files.

---
+++ main.c
@@ -0,0 +1,24 @@
+/*!
+ * \file            main.c
+ * \brief           Main function for cdatastruct.
+ * \details         Main function for cdatastruct.
+ * \author          Paul Griffiths
+ * \copyright       Copyright 2013 Paul Griffiths. Distributed under the terms
+ * of the GNU General Public License. <http://www.gnu.org/licenses/>
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*!
+ * \brief       Main function.
+ * \details     Main function.
+ * \returns     Exit status.
+ */
+
+int main(void) {
+
+    return EXIT_SUCCESS;
+}
