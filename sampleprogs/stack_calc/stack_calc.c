04b6c1fc316cf0cf1f0863e1ae7d43e5fc440552
Paul Griffiths <mail@paulgriffiths.net>
2013-09-08 15:40:30 -0400

    Add auto-generated files.

---
+++ sampleprogs/stack_calc/stack_calc.c
@@ -0,0 +1,24 @@
+/*!
+ * \file            stack_calc.c
+ * \brief           Entry point to stack_calc
+ * \author          Paul Griffiths
+ * \copyright       Copyright 2013 Paul Griffiths. Distributed under the terms
+ * of the GNU General Public License. <http://www.gnu.org/licenses/>
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*!
+ * \brief           `main()` function.
+ * \param argc      Number of command line arguments.
+ * \param argv      Command line arguments.
+ * \returns         Exit status.
+ */
+
+int main(int argc, char ** argv) {
+
+    return EXIT_SUCCESS;
+}
